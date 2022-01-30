4f6926a1a46f04249f6761d0da6a3f0cd5080682
yairshp <49451768+yairshp@users.noreply.github.com>
2022-01-30 19:49:00 +0200

    added to Task - 'isDone', 'labels', 'subTasks' - before testing

--- TaskManager/Task.cs
+++ TaskManager/Task.cs
@@ -24,6 +24,9 @@ public class Task
         // set => _creationTime = value;
     }
 
+    private bool _isDone;
+    public bool IsDone { get; }
+
     private TaskPriority _priority;
     public TaskPriority Priority
     {
@@ -44,15 +47,63 @@ public class Task
         get => _dueTime;
     }
 
+    private List<string> _labels;
+    public List<string> Labels
+    {
+        get => _labels;
+    }
+
+    private List<Task> _subTasks;
+
+    public List<Task> SubTasks
+    {
+        get => _subTasks;
+    }
+
     public Task(string title, 
                 TaskPriority priority = TaskPriority.Medium,
                 string? description = null,
-                DateTime? dueDate = null)
+                DateTime? dueDate = null,
+                List<string>? labels = null,
+                List<Task>? subTasks = null)
     {
+        _isDone = false;
         _title = title;
         _priority = priority;
         _creationTime = DateTime.Now;
         _description = description;
         _dueTime = dueDate;
+        _labels = labels ?? new List<string>();
+        _subTasks = subTasks ?? new List<Task>();
+    }
+
+    public void MarkAsDone()
+    {
+        _isDone = true;
+    }
+
+    public void MarkAsUndone()
+    {
+        _isDone = false;
+    }
+
+    public void AddLabel(string label)
+    {
+        this._labels.Add(label);
+    }
+
+    public void RemoveLabel(string label)
+    {
+        this._labels.Remove(label);
+    }
+
+    public void AddSubTask(Task subTask)
+    {
+        _subTasks.Add(subTask);
+    }
+
+    public void RemoveSubTask(Task subTask)
+    {
+        _subTasks.Remove(subTask);
     }
 }
