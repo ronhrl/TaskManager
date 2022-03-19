ea62d83756d63430f017d31ba60e0df31b2ac38e
Ron Harel <ron.harel@intel.com>
2022-03-20 01:22:04 +0200

    updating model and db

--- TaskManager/Models/ITaskMangerFilesStorage.cs
+++ TaskManager/Models/ITaskMangerFilesStorage.cs
@@ -3,11 +3,17 @@ namespace TaskManager.Models;
 public interface ITaskManagerFilesStorage
 {
     
-    public void InsertNewTaskM(Task task);
+    public void InsertNewTask(Task t);
 
-    public void DeleteTaskFromDbM(Task t);
+    public void DeleteTaskFromDb(Task t);
 
-    public void UpdateTaskInDbM(Task task);
+    public void UpdateTaskInDb(Task t);
 
-    public List<Task> GetTasksFromDbM();
+    public List<Task> GetTasksFromDb();
+
+    public void InsertNewSubTask(Task t, Task subTask);
+
+    public void DeleteSubTaskFromDb(Task subTask);
+
+    public void UpdateSubTaskInDb(Task task);
 }
